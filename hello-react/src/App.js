78ce95f863400a5cdf33c1d4dac7e6bffdba64b3
kusakina0608 <platina.kusakina@gmail.com>
2021-12-20 00:02:56 +0900

    자바스크립트 표현식에서 삼항 연산자 사용

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -4,8 +4,7 @@ function App() {
   const name = '리액트';
   return (
     <Fragment>
-      <h1>{name} 안녕!</h1>
-      <h2>잘 작동하니?</h2>
+      {name === '리액트' ? (<h1>리액트입니다.</h1>) : (<h2>리액트가 아닙니다.</h2>)}
     </Fragment>
   );
 }
