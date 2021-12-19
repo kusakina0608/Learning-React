2d86f318c3b35f416b1cedad1db14a4843fd995a
kusakina0608 <platina.kusakina@gmail.com>
2021-12-20 00:16:51 +0900

    style 객체 선언 없이 인라인 스타일링

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,12 @@
 function App() {
   const name = '리액트';
-  const style = {
+  return <div style={{
     backgroundColor: 'black',
     color: 'aqua',
     fontSize: '48px',
     fontWeight: 'bold',
     padding: 16
-  };
-  return <div style={style}>{name}</div>
+  }}>{name}</div>
 }
 
 export default App;
