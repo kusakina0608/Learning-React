f2d7553d0c16e19b868c617fdbe62f44f99f5c9e
kusakina0608 <platina.kusakina@gmail.com>
2021-12-20 15:27:50 +0900

    클래스 컴포넌트

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,8 +1,11 @@
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  const name = '리액트';
-  return <div>{name}</div>;
+class App extends Component {
+  render() {
+    const name = 'react';
+    return <div className="react">{name}</div>;
+  }
 }
 
 export default App;
