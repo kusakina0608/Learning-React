7e77b7e58b0a279f840c9863e96ca82258c7a200
kusakina0608 <platina.kusakina@gmail.com>
2021-12-29 09:16:41 +0900

    App 컴포넌트를 클래스 컴포넌트로 변경

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,7 +1,10 @@
+import { Component } from 'react';
 import ValidationSample from './ValidationSample';
 
-const App = () => {
-  return <ValidationSample />;
-};
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
 
 export default App;
