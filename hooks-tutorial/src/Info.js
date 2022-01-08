277c0ffe17614a14d81442a4862ec05e6abdac6c
kusakina0608 <platina.kusakina@gmail.com>
2022-01-08 23:25:10 +0900

    특정 값이 업데이트될 때만 실행되도록 변경

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -4,12 +4,8 @@ const Info = () => {
   const [nickname, setNickname] = useState("");
 
   useEffect(() => {
-    console.log("렌더링이 완료되었습니다!");
-    console.log({
-      name,
-      nickname,
-    });
-  }, []);
+    console.log(name);
+  }, [name]);
 
   const onChangeName = (e) => {
     setName(e.target.value);
