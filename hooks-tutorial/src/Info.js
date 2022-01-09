17c61823c2cf8b6a5092ccc92ed9dc620c86fb76
kusakina0608 <platina.kusakina@gmail.com>
2022-01-09 22:19:31 +0900

    언마운트 될 때만 뒷정리하기

--- hooks-tutorial/src/Info.js
+++ hooks-tutorial/src/Info.js
@@ -5,12 +5,10 @@ const Info = () => {
 
   useEffect(() => {
     console.log("effect");
-    console.log(name);
     return () => {
-      console.log("cleanup");
-      console.log(name);
+      console.log("unmount");
     };
-  }, [name]);
+  }, []);
 
   const onChangeName = (e) => {
     setName(e.target.value);
