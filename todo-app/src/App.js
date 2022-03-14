0efb88957100b69c5d97882b475eea464e870d08
kusakina0608 <platina.kusakina@gmail.com>
2022-03-14 09:01:29 +0900

    TodoTemplate 생성

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import TodoTemplate from './TodoTemplate';
 
 function App() {
-  return <div>Todo App을 만들자</div>;
+  return <TodoTemplate>Todo App을 만들자</TodoTemplate>;
 }
 
 export default App;
